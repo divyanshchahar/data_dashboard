d8f8faf3d02043966551f591f9d7c7f9fcf7096a
divyanshchahar <divyanshchahar@outlook.com>
2023-11-10 15:48:36 +0530

    added new componenet

---
+++ src/ui/components/AvatarComponent.jsx
@@ -0,0 +1,10 @@
+import { Avatar } from '@mui/material';
+import Face3Icon from '@mui/icons-material/Face3';
+
+import React from 'react';
+
+function AvatarComponent() {
+	return <Avatar src={Face3Icon} />;
+}
+
+export default AvatarComponent;
