c4bc6442c0fe32699705d4195b481267f1d00cae
divyanshchahar <divyanshchahar@outlook.com>
2023-11-16 21:07:20 +0530

    updated layout

--- src/ui/layouts/TopNavBar.jsx
+++ src/ui/layouts/TopNavBar.jsx
@@ -3,23 +3,34 @@ import { Box, AppBar, Toolbar } from '@mui/material';
 import assadiusLogo from '../../assets/assaduisLogo.png';
 import NotificationComponent from '../components/NotificationComponent';
 import SearchBar from '../components/SearchBar';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import IconButtonComponent from '../components/IconButtonComponent';
 
 function TopNavBar() {
 	return (
 		<>
 			<Box>
-				<AppBar>
-					<Toolbar sx={{ justifyContent: 'space-between' }}>
-						<Box sx={{ height: '50px' }}>
-							<img src={assadiusLogo} height="50px" />
+				<AppBar sx={{ boxShadow: 'none' }}>
+					<Toolbar
+						sx={{
+							justifyContent: 'space-between',
+							color: 'black',
+							backgroundColor: 'white',
+							alignItems: 'center',
+						}}
+					>
+						<Box sx={{ height: 'fit-content' }}>
+							<img src={assadiusLogo} height="25px" />
 						</Box>
 
-						<Box sx={{ display: 'flex' }}>
+						<Box sx={{ display: 'flex', alignItems: 'center', gap: '25px' }}>
 							<SearchBar />
 
 							<NotificationComponent />
 
 							<AvatarComponent />
+
+							<IconButtonComponent Icon={ArrowDropDownIcon} />
 						</Box>
 					</Toolbar>
 				</AppBar>
